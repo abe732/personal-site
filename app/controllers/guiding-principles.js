cbdc7230d0c264f344703cc8b0f1b2f291bd2f2a
Adam Behrens <behrens.adam@gmail.com>
2016-07-09 22:42:25 -0700

    [feat] add aangular controller shells

---
+++ app/controllers/guiding-principles.js
@@ -0,0 +1,5 @@
+angular.module('app.guiding-principles', [])
+
+.controller('GuidingPrinciplesController', function ($scope) {
+
+});
---
+++ app/controllers/skills.js
@@ -0,0 +1,5 @@
+angular.module('app.skills', [])
+
+.controller('SkillsController', function ($scope) {
+
+});
